bc3ed59133a825f4436bb827d49156a3b44157f1
ahmed waleed <68202088+medobedo33@users.noreply.github.com>
2024-01-22 14:18:28 +0200

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -31,15 +31,9 @@ def ceaser_decrypt(ciphertext: str, key: int):
         plaintext += alphabet[(alphabet.index(letter) - key) % 26]
     # ciphertext += chr((ord(letter) - key) % 128)
     return plaintext
-
-
 # OUTPUT
 ciphertext = ceaser_encrypt(plaintext, key)
 decrypted = ceaser_decrypt(ciphertext, key)
 print("====ceaser Encrytion demo===")
 print(f"plaintext: {plaintext}\nciphertext:{ciphertext}")
 print(f"decrypted: {decrypted}")
-
-git remote add origin https://github.com/medobedo33/cryptography_with-_-python.git
-git branch -M main
-git push -u origin main

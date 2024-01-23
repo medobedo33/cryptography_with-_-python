222947505a5a1d48de0f4bb459a85bed2a37ea9e
ahmed waleed <68202088+medobedo33@users.noreply.github.com>
2024-01-23 02:01:09 +0200

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,1 +1,8 @@
-from cipher import ceaser_decrypt 
+from cipher import ceaser_decrypt
+
+def ceaser_bruteforce(ciphertext:str):
+    for key in range(1,26):
+        print(f"{key}: {ceaser_decrypt(ciphertext,key)}")
+
+ciphertext = "khoor"
+ceaser_bruteforce(ciphertext)        
